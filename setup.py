2f08c0f3843d7ef870a5c25ffc60b99c329effe1
Aaron Margolese-Malin <aaron@thinkdeeply.com>
2021-04-12 08:51:28 -0400

    convert to installable package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='deep_sort_pytorch',
+    version='1.0',
+    packages=['deep_sort_pytorch'],
+    install_requires=[
+        "easydict",
+        "opencv-python"
+    ]
+)
